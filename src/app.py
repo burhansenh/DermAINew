1ab9cea9fd96b5734f30c458d520be814be767f9
Enes Yilmaz <enesyilmaz5157@gmail.com>
2024-11-07 20:44:27 -0500

    fixxed porting issue with 2 servers

--- src/app.py
+++ src/app.py
@@ -47,4 +47,4 @@ def detect_face_api():
     return jsonify({"processed_image": result_base64})
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, port=5000)
